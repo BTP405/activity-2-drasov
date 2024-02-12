48c3f75aace2f9ce544749741db7086505db3516
dan <117206336+drasov@users.noreply.github.com>
2024-02-12 23:05:51 +0000

    commiting

--- q1.py
+++ q1.py
@@ -1,1 +1,15 @@
 def getPrimeNumbers(n):
+    return [x for x in range(2, n+1) if checkPrime(x)]
+
+
+def checkPrime(numbers):
+    if numbers < 2:
+        return False
+    for i in range(2, int(numbers**0.5) + 1):
+        if numbers % i == 0:
+            return False
+    return True
+
+n = 50
+prime_numbers = getPrimeNumbers(n)
+print(f"Prime numbers between 2 and {n}: {prime_numbers}")
